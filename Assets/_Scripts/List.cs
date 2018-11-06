40befc4270be200ddb7b831a7ed21f78a7e4534c
Unknown <MXCapturing@gmail.com>
2018-11-06 15:20:47 +1300

    Item Stacking and 6 slot Inventory

---
+++ Assets/_Scripts/List.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class List : MonoBehaviour
+{
+    public Dictionary<string, int> itemDict;
+    public int number = -100;
+
+
+    private void Start()
+    {
+        itemDict = new Dictionary<string, int>();
+
+        /*itemDict.Add("Seeds", 5);
+        print("Seeds: " + itemDict["Seeds"]);
+        itemDict["Seeds"] += 2;
+        print(itemDict["Seeds"]);*/
+    }
+}    
