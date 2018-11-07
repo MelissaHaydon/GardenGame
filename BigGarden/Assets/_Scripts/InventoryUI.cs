d99e9e67b948f043064f8d663ee658895d72456f
Unknown <signupdump1@gmail.com>
2018-11-08 12:52:17 +1300

    Imported Connor's Scripts

---
+++ BigGarden/Assets/_Scripts/InventoryUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour {
+
+    public bool[] isFull;
+    public GameObject[] invSlot;
+    public int[] itemNum;
+    public string[] itemName;
+    public Text[] itemAmount;
+    public Dictionary<string, int> itemDict;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        itemDict = new Dictionary<string, int>();
+    }
+    private void Update()
+    {
+        for (int i = 0; i < invSlot.Length; i++)
+        {
+            itemAmount[i].text = "" + itemNum[i];
+        }
+    }
+}
---
+++ BigGarden/Assets/_Scripts/PickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    private InventoryUI inventory;
+    public GameObject itemButton;
+    public string itemName;
+
+	// Use this for initialization
+	void Start () {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryUI>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            for (int i = 0; i < inventory.invSlot.Length; i++)
+            {
+                if(inventory.isFull[i] == false && inventory.itemDict.ContainsKey(itemName) == false)
+                {
+                    inventory.isFull[i] = true;
+                    inventory.itemName[i] = itemName;
+                    inventory.itemNum[i]++;
+                    inventory.itemDict.Add(itemName, 1);
+                    Instantiate(itemButton, inventory.invSlot[i].transform, false);             
+                    Destroy(gameObject);
+                    break;
+                }
+                else if (inventory.itemDict.ContainsKey(itemName) == true && inventory.itemName[i] == itemName)
+                {
+                    inventory.itemNum[i]++;
+                    inventory.itemDict[itemName] += 1;
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
