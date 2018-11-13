72adf807d1f12ecf25ec5569e197f8cbfad15c29
Unknown <signupdump1@gmail.com>
2018-11-14 12:53:37 +1300

    Added Sprite Shadow Material + Script
    Disables sprites from being flipped, though. Need to fix backface culling.

---
+++ BigGarden/Assets/_Scripts/Jae_SpriteShadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jae_SpriteShadow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer == null) Debug.Log("Renderer is empty");
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
+        GetComponent<Renderer>().receiveShadows = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
