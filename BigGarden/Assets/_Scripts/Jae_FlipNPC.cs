0dbbb27a0f5d1dc81c1777b3471989ffe174db12
Unknown <signupdump1@gmail.com>
2018-11-14 13:30:37 +1300

    Added FlipNPC.css for Bee

---
+++ BigGarden/Assets/_Scripts/Jae_FlipNPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jae_FlipNPC : MonoBehaviour {
+
+    public Jae_VIDEPlayerScript player;
+    private Animator anim;
+    public int playerDirection;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<Jae_VIDEPlayerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.x > transform.position.x)
+        {
+            playerDirection = 1;
+        }
+        else if (player.transform.position.x < transform.position.x)
+        {
+            playerDirection = -1;
+        }
+
+        anim.SetInteger("PlayerDirection", playerDirection);
+	}
+}
