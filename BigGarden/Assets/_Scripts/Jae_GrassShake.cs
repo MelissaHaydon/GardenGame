f54f962a870837ae25cf80aa2159d2b7000e5fbb
Unknown <signupdump1@gmail.com>
2018-11-15 12:58:56 +1300

    Added GrassShake.css

---
+++ BigGarden/Assets/_Scripts/Jae_GrassShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jae_GrassShake : MonoBehaviour {
+    
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("ShakeTrig");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("ShakeTrig");
+        }
+    }
+}
