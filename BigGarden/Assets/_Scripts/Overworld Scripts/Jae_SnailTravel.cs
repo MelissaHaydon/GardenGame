aed400fcbbe2518f208a5ddad5133fbc4028e715
Unknown <signupdump1@gmail.com>
2018-11-20 09:46:38 +1300

    Snail Actually Moves when Told To

---
+++ BigGarden/Assets/_Scripts/Overworld Scripts/Jae_SnailTravel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jae_SnailTravel : MonoBehaviour {
+
+    public Jae_FlipNPC flipNPC;
+    public int playerDirection;
+    public Animator anim;
+    public float moveSpeed;
+    public bool moving;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving)
+        {
+            transform.position += new Vector3(moveSpeed, 0, 0) * Time.deltaTime;
+        }
+	}
+
+    public void StartTravel()
+    {
+        flipNPC.enabled = false;
+        playerDirection = 1;
+        anim.SetInteger("PlayerDirection", playerDirection);
+        moving = true;
+    }
+}
