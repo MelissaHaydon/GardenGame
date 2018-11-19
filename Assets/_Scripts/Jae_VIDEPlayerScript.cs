8cd3595f1fb307eaf084fcea2f09301d70f36a58
Unknown <MXCapturing@gmail.com>
2018-11-20 12:31:13 +1300

    Menu Complete
    - Audio needs testing
    - Credits not done

---
+++ Assets/_Scripts/Jae_VIDEPlayerScript.cs
@@ -0,0 +1,213 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using VIDE_Data;
+
+public class Jae_VIDEPlayerScript : MonoBehaviour
+{
+    //This script handles player movement and interaction with other NPC game objects
+
+    public string playerName;
+
+    // public Jae_GameManager gameManager;
+
+    //Reference to our diagUI script for quick access
+    //public UIManager diagUI;
+    //public QuestChartDemo questUI;
+    public Animator anim;
+
+    //Stored current VA when inside a trigger
+    // public VIDE_Assign inTrigger;
+
+    public GameObject attentionBubble;
+
+    public float z;
+    public float x;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode actionKey;
+    public KeyCode runKey;
+
+    public float moveSpeed;
+    public bool moving;
+    public Vector2 lastMove;
+    public float moveAnimSpeed;
+
+    public bool isSprinting;
+    public ParticleSystem dustParts;
+
+    //public Jae_CameraTracker playerCamera;
+
+    void OnTriggerEnter(Collider other)
+    {
+        /*if (other.GetComponent<VIDE_Assign>() != null)
+        {
+            inTrigger = other.GetComponent<VIDE_Assign>();
+            attentionBubble.SetActive(true);
+        }*/
+    }
+
+    void OnTriggerExit()
+    {
+        //inTrigger = null;
+        attentionBubble.SetActive(false);
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        //Only allow player to move and turn if there are no dialogs loaded
+        //if (!VD.isActive)
+        //{
+        if (Input.GetKey(upKey))
+        {
+            z = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+            transform.Translate(0, 0, z);
+            moving = true;
+        }
+        if (Input.GetKey(downKey))
+        {
+            z = Input.GetAxisRaw("Vertical") * -moveSpeed * Time.deltaTime;
+            transform.Translate(0, 0, -z);
+            moving = true;
+        }
+        if (Input.GetKey(leftKey))
+        {
+            x = Input.GetAxisRaw("Horizontal") * -moveSpeed * Time.deltaTime;
+            transform.Translate(-x, 0, 0);
+            moving = true;
+        }
+        if (Input.GetKey(rightKey))
+        {
+            x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            transform.Translate(x, 0, 0);
+            moving = true;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), lastMove.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            lastMove = new Vector2(lastMove.x, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (!Input.GetKey(upKey) && !Input.GetKey(downKey) && !Input.GetKey(leftKey) && !Input.GetKey(rightKey))
+        {
+            moving = false;
+        }
+
+        /*if (Input.GetKey(runKey) && moving)
+        {
+            moveSpeed = 9;
+            anim.speed = 2;
+            //playerCamera.defaultDistance.z = -9;
+            dustParts.gameObject.SetActive(true);
+            if (lastMove.x == 1 && Input.GetKey(rightKey))
+            {
+                // playerCamera.defaultDistance.x = 2;
+            }
+            else if (lastMove.x == -1 && Input.GetKey(leftKey))
+            {
+                //playerCamera.defaultDistance.x = -2;
+            }
+            else
+            {
+                //playerCamera.defaultDistance.x = 0;
+
+            } }
+        else
+        {
+            moveSpeed = 5;
+            anim.speed = 1;
+            dustParts.gameObject.SetActive(false);
+            // playerCamera.defaultDistance.z = -8;
+            // playerCamera.defaultDistance.x = 0;
+        }
+
+       // }
+
+        //Interact with NPCs when pressing E
+        if (Input.GetKeyDown(actionKey))
+        {
+            if (!moving)
+            {
+                //TryInteract();
+            }
+        }
+
+        if (dustParts.gameObject.activeSelf)
+        {
+            if (Input.GetKeyDown(downKey))
+            {
+                dustParts.gameObject.transform.localEulerAngles = new Vector3(-25, 0, 0);
+            }
+            if (Input.GetKeyDown(leftKey))
+            {
+                dustParts.gameObject.transform.localEulerAngles = new Vector3(-25, 90, 0);
+            }
+            if (Input.GetKeyDown(upKey))
+            {
+                dustParts.gameObject.transform.localEulerAngles = new Vector3(-25, 180, 0);
+            }
+            if (Input.GetKeyDown(rightKey))
+            {
+                dustParts.gameObject.transform.localEulerAngles = new Vector3(-25, -90, 0);
+            }
+        }
+
+        if (transform.position.y <= -10)
+        {
+            transform.position = new Vector3(-11.95f, 0, 0);
+        }
+
+        anim.SetBool("Moving", moving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    //Casts a ray to see if we hit an NPC and, if so, we interact
+   // void TryInteract()
+    {
+        /* Prioritize triggers */
+
+        // if (inTrigger)
+        //{
+        // diagUI.Interact(inTrigger);
+        // return;
+        //}
+
+        /* If we are not in a trigger, try with raycasts */
+
+        //RaycastHit rHit;
+
+        //if (Physics.Raycast(transform.position, transform.forward, out rHit, 2))
+        //{
+        //    //Lets grab the NPC's VIDE_Assign script, if there's any
+        //    VIDE_Assign assigned;
+        //    if (rHit.collider.GetComponent<VIDE_Assign>() != null)
+        //        assigned = rHit.collider.GetComponent<VIDE_Assign>();
+        //    else return;
+
+        //    if (assigned.alias == "QuestUI")
+        //    {
+        //        questUI.Interact(); //Begins interaction with Quest Chart
+        //    }
+        //    else
+        //    {
+        //        diagUI.Interact(assigned); //Begins interaction
+        //    }
+        //}
+        //}
+    }
+}
--- Assets/_Scripts/Menus/Options.cs
+++ Assets/_Scripts/Menus/Options.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Options : MonoBehaviour {
+
+    public Jae_VIDEPlayerScript movement;
 
     public Slider volumeS;
     public float volume;
     public Text volumeT;
 
     public Dropdown resolution;
-    public bool fullScreen;
+    public Toggle fullScreen;
 
-	// Use this for initialization
-	void Start () {
+    public Button up; public Button down; public Button left; public Button right; public Button interact; public Button sprint;
+    public string buttonPushed;
+    public GameObject controlPanel;
+
+    // Use this for initialization
+    void Start () {
+        movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Jae_VIDEPlayerScript>();
+
         volumeS.value = 50;
-        fullScreen = false;
+        fullScreen.isOn = false;
 	}
 	
 	// Update is called once per frame
@@ -24,50 +33,186 @@ public class Options : MonoBehaviour {
         volumeT.text = "" + volume;
         AudioListener.volume = volume;
 
-        if (Input.GetKeyDown(KeyCode.P))
+        if(controlPanel.activeSelf == true)
         {
-            Debug.Log(AudioListener.volume);
+            #region up
+            if (Input.inputString != "" && buttonPushed == "up")
+            {
+                GameObject.Find("UpButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.upKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "up")
+            {
+                GameObject.Find("UpButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.upKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "up")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
+            #region down
+            if (Input.inputString != "" && buttonPushed == "down")
+            {
+                GameObject.Find("DownButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.downKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "down")
+            {
+                GameObject.Find("DownButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.downKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "down")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
+            #region left
+            if (Input.inputString != "" && buttonPushed == "left")
+            {
+                GameObject.Find("LeftButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.leftKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "left")
+            {
+                GameObject.Find("LeftButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.leftKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "left")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
+            #region right
+            if (Input.inputString != "" && buttonPushed == "right")
+            {
+                GameObject.Find("RightButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.rightKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "right")
+            {
+                GameObject.Find("RightButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.rightKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "right")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
+            #region interact
+            if (Input.inputString != "" && buttonPushed == "interact")
+            {
+                GameObject.Find("InteractButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.actionKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "interact")
+            {
+                GameObject.Find("InteractButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.actionKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "interact")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
+            #region sprint
+            if (Input.inputString != "" && buttonPushed == "sprint")
+            {
+                GameObject.Find("SprintButton").GetComponentInChildren<Text>().text = Input.inputString.ToUpper();
+                movement.runKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Input.inputString.ToUpper());
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftControl) && buttonPushed == "sprint")
+            {
+                GameObject.Find("SprintButton").GetComponentInChildren<Text>().text = "Left Control";
+                movement.runKey = KeyCode.LeftControl;
+                controlPanel.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape) && buttonPushed == "sprint")
+            {
+                controlPanel.SetActive(false);
+            }
+            #endregion
         }
-	}
-
-    public void FullScreen()
-    {
-        fullScreen = !fullScreen;
     }
 
     public void ScreenRes()
     {
-        if (resolution.value == 0 && fullScreen == false)
+        if (resolution.value == 0 && fullScreen.isOn == false)
         {
             Screen.SetResolution(1024, 768, false);
         }
-        if (resolution.value == 0 && fullScreen == true)
+        if (resolution.value == 0 && fullScreen.isOn == true)
         {
             Screen.SetResolution(1024,768, true);
         }
-        if (resolution.value == 1 && fullScreen == true)
+        if (resolution.value == 1 && fullScreen.isOn == true)
         {
             Screen.SetResolution(1366, 768, true);
         }
-        if (resolution.value == 1 && fullScreen == false)
+        if (resolution.value == 1 && fullScreen.isOn == false)
         {
             Screen.SetResolution(1366, 768, false);
         }
-        if (resolution.value == 2 && fullScreen == true)
+        if (resolution.value == 2 && fullScreen.isOn == true)
         {
-            Screen.SetResolution(960, 640, true);
+            Screen.SetResolution(1920, 1200, true);
         }
-        if (resolution.value == 2 && fullScreen == false)
+        if (resolution.value == 2 && fullScreen.isOn == false)
         {
-            Screen.SetResolution(960, 640, false);
+            Screen.SetResolution(1920, 1200, false);
         }
-        if (resolution.value == 3 && fullScreen == true)
+        if (resolution.value == 3 && fullScreen.isOn == true)
         {
             Screen.SetResolution(1920, 1080, true);
         }
-        if (resolution.value == 3 && fullScreen == false)
+        if (resolution.value == 3 && fullScreen.isOn == false)
         {
             Screen.SetResolution(1920, 1080, false);
+        }
+    }
+
+    public void OnClickedButton()
+    {
+       if(EventSystem.current.currentSelectedGameObject.name == "UpButton")
+       {
+            controlPanel.SetActive(true);
+            buttonPushed = "up";
+       }
+        if (EventSystem.current.currentSelectedGameObject.name == "DownButton")
+        {
+            controlPanel.SetActive(true);
+            buttonPushed = "down";
+        }
+        if (EventSystem.current.currentSelectedGameObject.name == "LeftButton")
+        {
+            controlPanel.SetActive(true);
+            buttonPushed = "left";
+        }
+        if (EventSystem.current.currentSelectedGameObject.name == "RightButton")
+        {
+            controlPanel.SetActive(true);
+            buttonPushed = "right";
+        }
+        if (EventSystem.current.currentSelectedGameObject.name == "InteractButton")
+        {
+            controlPanel.SetActive(true);
+            buttonPushed = "interact";
+        }
+        if (EventSystem.current.currentSelectedGameObject.name == "SprintButton")
+        {
+            controlPanel.SetActive(true);
+            buttonPushed = "sprint";
         }
     }
 }
