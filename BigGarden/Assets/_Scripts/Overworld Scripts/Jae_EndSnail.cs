99fdf4fa60e01e3f953b4cda0e9df71328af1df3
Unknown <signupdump1@gmail.com>
2018-12-05 12:43:26 +1300

    Added Snail to End Screen

---
+++ BigGarden/Assets/_Scripts/Overworld Scripts/Jae_EndSnail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jae_EndSnail : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x >= 150)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
